0428f010f1a79d9e153a4ec6aa1042f59ae510ce
Charan <charanmr0717@gmail.com>
2026-07-13 22:33:00 +0530

    day 2: practiced data types

---
+++ day 3/datatypes.py
@@ -0,0 +1,31 @@
+# 1. Integer (whole numbers)
+age = 18
+
+# 2. Float (decimal numbers)
+height = 5.9
+
+# 3. String (text)
+name = "Charan"
+
+# 4. Boolean (True/False)
+is_student = True
+
+# 5. List (collection, changeable)
+subjects = ["Physics", "Chemistry", "Maths"]
+
+# 6. Tuple (collection, unchangeable)
+coordinates = (10, 20)
+
+# 7. Dictionary (key-value pairs)
+student = {"name": "Charan", "age": 18}
+
+# types
+age = 17
+height = 5.9
+name = "charan"
+is_student = True 
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
